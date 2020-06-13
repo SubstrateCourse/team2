48f86b8e8f6c59d08a10b71deb0242f686d5d15f
wjsxqs <xsmlcp@163.com>
2020-06-13 17:54:38 +0800

    Homework for lesson 3

---
+++ lesson3/substrate-front-end-template/src/PoeModule.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Grid, Card, Statistic } from 'semantic-ui-react';
+
+import { useSubstrate } from './substrate-lib';
+import { TxButton } from './substrate-lib/components';
+import { blake2AsHex } from '@polkadot/util-crypto';
+
+function Main (props) {
+  const { api } = useSubstrate();
+  const { accountPair } = props;
+
+  // The transaction submission status
+  const [status, setStatus] = useState('');
+
+  // The currently stored value
+  const [digest, setDigest] = useState('');
+  const [owner, setOwner] = useState('');
+  const [blockNumber, setBlockNumber] = useState('');
+  const [dest, setDest] = useState('');
+
+  useEffect(() => {
+    let unsubscribe;
+    api.query.poeModule.proofs(digest, (result) => {
+      console.log(result);
+      setOwner(result[0].toString());
+      setBlockNumber(result[1].toNumber());
+
+    }).then(unsub => {
+      unsubscribe = unsub;
+    })
+      .catch(console.error);
+
+    return () => unsubscribe && unsubscribe();
+  }, [digest, api.query.poeModule]);
+
+  const handleFileChosen = (file) => {
+    let fileReader = new FileReader();
+
+    const bufferToDigest = () => {
+      const content = Array.from(new Uint8Array(fileReader.result))
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+
+      const hash = blake2AsHex(content, 256);
+      setDigest(hash);
+    }
+
+    fileReader.onloadend = bufferToDigest;
+
+    fileReader.readAsArrayBuffer(file);
+  }
+
+  const handleDestChange = (dest) => {
+    setDest(dest);
+  }
+
+  return (
+    <Grid.Column width={8}>
+      <h1>Proof of Existence Module</h1>
+      <Form>
+        <Form.Field>
+          <Input
+            type='file'
+            id='file'
+            label='New File'
+            onChange={ (e) => handleFileChosen(e.target.files[0]) }
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <TxButton
+            accountPair={accountPair}
+            label='Create Claim'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'poeModule',
+              callable: 'createClaim',
+              inputParams: [digest],
+              paramFields: [true]
+            }}
+          />
+          <TxButton
+            accountPair={accountPair}
+            label='Revoke Claim'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'poeModule',
+              callable: 'revokeClaim',
+              inputParams: [digest],
+              paramFields: [true]
+            }}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type='text'
+            id='dest'
+            label='Dest'
+            onChange={(_, { value }) => handleDestChange(value)}
+          />
+          <TxButton
+            accountPair={accountPair}
+            label='Transfer Claim'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'poeModule',
+              callable: 'transferClaim',
+              inputParams: [digest, dest],
+              paramFields: [true]
+            }}
+          />
+        </Form.Field>
+
+        <div>{status}</div>
+        <div>{`Claim info, owner: ${owner}, blockNumber: ${blockNumber}, dest: ${dest}`}</div>
+      </Form>
+    </Grid.Column>
+  );
+}
+
+export default function PoeModule (props) {
+  const { api } = useSubstrate();
+  return (api.query.poeModule && api.query.poeModule.proofs
+    ? <Main {...props} /> : null);
+}
