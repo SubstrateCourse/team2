5273bababb830283ab938b304557c03d7da253ca
Jason Wang <starlure@gmail.com>
2020-07-04 13:27:19 -0700

    basic question for homework 8

--- lesson8/substrate-node-template/pallets/template/src/lib.rs
+++ lesson8/substrate-node-template/pallets/template/src/lib.rs
@@ -9,8 +9,16 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{debug, decl_module, decl_storage, decl_event, decl_error, dispatch};
-use frame_system::{self as system, ensure_signed};
+use core::{convert::TryInto};
+use frame_support::{debug, decl_module, decl_storage, decl_event, decl_error, dispatch::DispatchResult};
+use frame_system::{self as system, ensure_signed,
+				   offchain::{
+					   AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer,
+				   },
+};
+use sp_core::crypto::KeyTypeId;
+use sp_std::prelude::*;
+use sp_std::str;
 
 #[cfg(test)]
 mod mock;
@@ -18,10 +26,48 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// The pallet's configuration trait.
-pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+pub const NUM_VEC_LEN: usize = 10;
 
+/// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrappers.
+/// We can use from supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
+/// the types with this pallet-specific identifier.
+pub mod crypto {
+	use crate::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+	use sp_core::sr25519::Public as Sr25519Public;
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519},
+		traits::Verify,
+		MultiSignature, MultiSigner,
+	};
+
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = Sr25519Signature;
+		type GenericPublic = Sr25519Public;
+	}
+
+	// implemented for mock runtime in test
+	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
+	for TestAuthId
+	{
+		type RuntimeAppPublic = Public;
+		type GenericSignature = Sr25519Signature;
+		type GenericPublic = Sr25519Public;
+	}
+}
+
+/// The pallet's configuration trait.
+pub trait Trait: system::Trait + CreateSignedTransaction<Call<Self>> {
+	// Add other types and constants required to configure this pallet.
+	type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+	/// The overarching dispatch call type.
+	type Call: From<Call<Self>>;
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
@@ -35,7 +81,7 @@ decl_storage! {
 		// Just a dummy storage item.
 		// Here we are declaring a StorageValue, `Something` as a Option<u32>
 		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
+		Numbers get(fn numbers): Vec<u64>;
 	}
 }
 
@@ -45,7 +91,7 @@ decl_event!(
 		/// Just a dummy event.
 		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
 		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
+		NewNumber(Option<AccountId>, u64),
 	}
 );
 
@@ -56,6 +102,8 @@ decl_error! {
 		NoneValue,
 		/// Value reached maximum and cannot be incremented further
 		StorageOverflow,
+		/// Error returned when making signed transactions in off-chain worker
+		SignedSubmitNumberError,
 	}
 }
 
@@ -72,8 +120,15 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
+		#[weight = 0]
+		pub fn submit_number_signed(origin, number: u64) -> DispatchResult {
+			debug::info!("submit_number_signed: {:?}", number);
+			let who = ensure_signed(origin)?;
+			Self::append_or_replace_number(Some(who), number)
+		}
+
 		#[weight = 10_000]
-		pub fn save_number(origin, number: u32) -> dispatch::DispatchResult {
+		pub fn save_number(origin, number: u32) -> DispatchResult {
 			// Check it was signed and get the signer. See also: ensure_root and ensure_none
 			let who = ensure_signed(origin)?;
 
@@ -87,10 +142,74 @@ decl_module! {
 		fn offchain_worker(block_number: T::BlockNumber) {
 			debug::info!("Entering off-chain workers");
 
-			/*******
-			 * 学员们在这里追加逻辑
-			 *******/
+			let result = Self::signed_submit_number(block_number);
+
+			if let Err(e) = result { debug::error!("Error: {:?}", e); }
 		}
 
+	}
+}
+
+impl<T: Trait> Module<T> {
+	/// Add a new number to the list.
+	fn append_or_replace_number(who: Option<T::AccountId>, number: u64) -> DispatchResult {
+		Numbers::mutate(|numbers| {
+			// The append or replace logic. The `numbers` vector is at most `NUM_VEC_LEN` long.
+			let num_len = numbers.len();
+
+			if num_len < NUM_VEC_LEN {
+				numbers.push(number);
+			} else {
+				numbers[num_len % NUM_VEC_LEN] = number;
+			}
+
+			// displaying the average
+			let num_len = numbers.len();
+			let average = match num_len {
+				0 => 0,
+				_ => numbers.iter().sum::<u64>() / (num_len as u64),
+			};
+
+			debug::info!("Current average of numbers is: {}", average);
+		});
+
+		// Raise the NewNumber event
+		Self::deposit_event(RawEvent::NewNumber(who, number));
+		Ok(())
+	}
+
+	fn signed_submit_number(block_number: T::BlockNumber) -> Result<(), Error<T>> {
+		let signer = Signer::<T, T::AuthorityId>::all_accounts();
+		if !signer.can_sign() {
+			debug::error!("No local account available");
+			return Err(<Error<T>>::SignedSubmitNumberError);
+		}
+
+		// Using `SubmitSignedTransaction` associated type we create and submit a transaction
+		// representing the call, we've just created.
+		// Submit signed will return a vector of results for all accounts that were found in the
+		// local keystore with expected `KEY_TYPE`.
+		let submission: u64 = block_number.try_into().ok().unwrap() as u64;
+		let results = signer.send_signed_transaction(|_acct| {
+			// We are just submitting the current block number back on-chain
+			Call::submit_number_signed(submission)
+		});
+
+		for (acc, res) in &results {
+			match res {
+				Ok(()) => {
+					debug::native::info!(
+						"off-chain send_signed: acc: {:?}| number: {}",
+						acc.id,
+						submission
+					);
+				}
+				Err(e) => {
+					debug::error!("[{:?}] Failed in signed_submit_number: {:?}", acc.id, e);
+					return Err(<Error<T>>::SignedSubmitNumberError);
+				}
+			};
+		}
+		Ok(())
 	}
 }
